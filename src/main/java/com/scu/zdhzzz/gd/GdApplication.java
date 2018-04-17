e890a907e5b41c584ee560fedca108b6aeddd012
Jack Hu <992264822@qq.com>
2018-04-17 14:48:34 +0800

    initialize prod profile

--- src/main/java/com/scu/zdhzzz/gd/GdApplication.java
+++ src/main/java/com/scu/zdhzzz/gd/GdApplication.java
@@ -1,12 +1,14 @@
 package com.scu.zdhzzz.gd;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GdApplication {
 
 	@RequestMapping("/")
@@ -15,6 +17,8 @@ public class GdApplication {
 		return "hello spring boot";
 	}
 	public static void main(String[] args) {
-		SpringApplication.run(GdApplication.class, args);
+		SpringApplication app = new SpringApplication(GdApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }
